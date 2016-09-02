8eb44202505abe90df848b033b56034122ede41f
Charles Clemens <cclemensjr@gmail.com>
2016-09-02 05:46:16 -0700

    Added some functionality to the button

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -1,25 +1,50 @@
 //The number of rows and columns to be added to the grid
-var numOfCells = 16;
+var numBlocks = 5;
 
-//Create a table with JS and attach it to the html
-var table = document.getElementById('screenTable');
-
-//Create an empty rows and add it to the table
-for (i = 0; i < numOfCells; i++)
-{
-  var row = table.insertRow(i);
-
-  //Add cells to the row
-  for (j = 0; j < numOfCells; j++)
+//function used to create the etch-a-sketch grid
+function createGrid(number) {
+  //clear the previous table
+  $('tr').remove();
+  //Create a table with JS and attach it to the html
+  var table = document.getElementById('screenTable');
+  //Create an empty row and add it to the table
+  for (i = 0; i < number; i++)
   {
-    var cell = row.insertCell(j);
-    cell.innerHTML = j;
+    var row = table.insertRow(i);
+    //Add columns to the row
+    for (j = 0; j < number; j++)
+    {
+      var cell = row.insertCell(j);
+    }
   }
 }
 
-//create a jquery script to add a color change effect when hovering over a square.
+createGrid(numBlocks);
+
 $(document).ready(function() {
+  //create a jquery script to add a color change effect when hovering over a square.
   $("td").hover(function(){
     $(this).addClass('hoverTD');
   });
+
+  //When the user clicks the button...
+  $("#setBlocks").on('click', function() {
+    //...reset the grid to it's default state...
+    $('td').addClass('resetTable');
+    //$('td').remove();
+  });
 });
+
+//and prompt for the new number of blocks per side
+function promptBlocks(){
+  var userBlocks = prompt("How many squares per side, young master?", "16");
+
+  //check to see if a number was received
+  if (isNaN(userBlocks))
+  {
+    alert("You must put in a number");
+    return false;
+  }
+
+  createGrid(userBlocks);
+}
